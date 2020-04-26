2ebfdaf750852f7b0b1af030c36efff6cfbb1614
harsewaksingh <13harsewak@gmail.com>
2020-04-26 03:20:15 +0100

    initial tests

---
+++ src/tests/userManager.test.ts
@@ -0,0 +1,46 @@
+import { expect } from "chai";
+import { UserManager } from "../data/userManager";
+import { Message } from "../api/apis/userApi";
+import { LoginRequest, RegisterRequest, Subscriber } from "../api/models";
+import { User } from "../data/models";
+import isString from "subscriptions-transport-ws/dist/utils/is-string";
+
+class MockedUserManager implements UserManager {
+  allMessages(): Promise<Array<Message>> {
+    return new Promise<Array<Message>>((resolve, rejects) => {
+      resolve([]);
+    });
+  }
+
+  login(loginRequest: LoginRequest): Promise<User> {
+    return new Promise<User>((resolve, rejects) => {
+      resolve({ firstName: "", lastName: "", email: "" });
+    });
+  }
+
+  logout(): Promise<Boolean> {
+    return new Promise<Boolean>((resolve, rejects) => {
+      resolve(true);
+    });
+  }
+
+  messages(subscriber: Subscriber<Message>): void {}
+
+  register(registerRequest: RegisterRequest): Promise<User> {
+    return new Promise<User>((resolve, rejects) => {
+      resolve({ firstName: "", lastName: "", email: "" });
+    });
+  }
+
+  welcome(): void {}
+}
+
+const userManager = new MockedUserManager();
+
+describe("UserManager", () => {
+  describe("login failure", () => {
+    let result = userManager.login({ email: "", password: "" });
+    console.log("==" + result);
+    expect(result).not.be.a.instanceOf(Promise.constructor);
+  });
+});
